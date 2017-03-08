a2cf4887e2a93c2b564049e183db2f358766b985
Sergey Seroukhov <seroukhov@gmail.com>
2017-03-08 16:51:14 -0700

    'Modified build to set version in bower'

--- component.build.ps1
+++ component.build.ps1
@@ -1,3 +1,6 @@
-task PreInstallDep -Before InstallDep {
+task AfterInstallDep -After InstallDep {
     Install-BowerDependencies -Path .
+}
+task AfterSetVetsion -After SetVersion {
+    Set-BowerVersion -Path . -Version $Version
 }
